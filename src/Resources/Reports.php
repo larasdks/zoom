5f86afbb2cf6a9d3fd8204371cabf5f19baad64b
Hamza Alayed <developerh108@gmail.com>
2026-01-14 16:56:47 +0300

    :sparkles: refactor meeting participants method and update documentation
    Signed-off-by: Hamza Alayed <developerh108@gmail.com>

--- src/Resources/Reports.php
+++ src/Resources/Reports.php
@@ -2,11 +2,12 @@
 
 namespace laraSDKs\Zoom\Resources;
 
+use Illuminate\Http\Client\ConnectionException;
 use laraSDKs\Zoom\Client;
 use laraSDKs\Zoom\Exceptions\ZoomApiException;
 
 /**
- * Service class for Zoom Reports resource.
+ * Service class for Zoom Meetings resource.
  */
 class Reports
 {
@@ -17,86 +18,15 @@ class Reports
         $this->client = $client;
     }
 
-    /**
-     * Get meeting participants report.
-     *
-     * @throws ZoomApiException
-     */
-    public function getMeetingParticipants(int $meetingId, array $query = []): array
-    {
-        return $this->client->request('get', "report/meetings/{$meetingId}/participants", [
-            'query' => $query,
-        ])['data'];
-    }
 
     /**
-     * Get meeting detail report.
+     * List all meeting participants for a meeting.
      *
-     * @throws ZoomApiException
+     * @throws ZoomApiException|ConnectionException
      */
-    public function getMeetingDetail(int $meetingId, array $query = []): array
+    public function meetingParticipants(string $meetingId, array $query = []): array
     {
-        return $this->client->request('get', "report/meetings/{$meetingId}", [
-            'query' => $query,
-        ])['data'];
-    }
-
-    /**
-     * Get daily usage report.
-     *
-     * @throws ZoomApiException
-     */
-    public function getDailyUsage(array $query = []): array
-    {
-        return $this->client->request('get', 'report/daily', [
-            'query' => $query,
-        ])['data'];
-    }
-
-    /**
-     * Get webinar participants report.
-     *
-     * @throws ZoomApiException
-     */
-    public function getWebinarParticipants(int $webinarId, array $query = []): array
-    {
-        return $this->client->request('get', "report/webinars/{$webinarId}/participants", [
-            'query' => $query,
-        ])['data'];
-    }
-
-    /**
-     * Get webinar detail report.
-     *
-     * @throws ZoomApiException
-     */
-    public function getWebinarDetail(int $webinarId, array $query = []): array
-    {
-        return $this->client->request('get', "report/webinars/{$webinarId}", [
-            'query' => $query,
-        ])['data'];
-    }
-
-    /**
-     * Get user activity report.
-     *
-     * @throws ZoomApiException
-     */
-    public function getUserActivity(array $query = []): array
-    {
-        return $this->client->request('get', 'report/users', [
-            'query' => $query,
-        ])['data'];
-    }
-
-    /**
-     * Get user activity report for a specific user.
-     *
-     * @throws ZoomApiException
-     */
-    public function getUserActivityByUser(string $userId, array $query = []): array
-    {
-        return $this->client->request('get', "report/users/{$userId}", [
+        return $this->client->request('get', "/report/meetings/$meetingId/participants", [
             'query' => $query,
         ])['data'];
     }
